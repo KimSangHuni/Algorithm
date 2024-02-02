b92157a00896473be636310ae78e41fd6fd567fd
KimSangHuni <158068697+KimSangHuni@users.noreply.github.com>
2024-02-02 13:04:07 +0900

    [level 3] Title: 산 모양 타일링, Time: 7.05 ms, Memory: 42.1 MB -BaekjoonHub

--- 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.js
+++ 프로그래머스/3/258705. 산 모양 타일링/산 모양 타일링.js
@@ -1,11 +1,12 @@
 function solution(n, tops) {
+    const DIV = 10007;
     let a = [1];
     let b = [2 + tops[0]];
     
     for(let i = 1; i < n; i++) {
-        a[i] = (a[i-1] + b[i-1]) % 10007;
-        b[i] = ((1 + tops[i]) * a[i-1] + (2 + tops[i]) * b[i-1]) % 10007;
+        a[i] = (a[i-1] + b[i-1]) % DIV;
+        b[i] = ((1 + tops[i]) * a[i-1] + (2 + tops[i]) * b[i-1]) % DIV;
     }
     
-    return (a[n-1] + b[n-1]) % 10007;
+    return (a[n-1] + b[n-1]) % DIV;
 }
