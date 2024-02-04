0a05ab104263db088e3b7ef5c7fa42cd0e324ecd
KimSangHuni <158068697+KimSangHuni@users.noreply.github.com>
2024-02-04 17:01:27 +0900

    Update 올바른 괄호.js

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.js
@@ -1,21 +1,15 @@
-function solution(s){
-    var answer = true;
+function solution(s) {
     const stack = [];
-    for(let i = 0 ; i < s.length; i++) {
-        if(stack.length === 0) {
-            if(s[i] === `)`) return false;
-            else stack.push(`(`);
-        }
+
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === "(") {
+            stack.push("(");
+        } 
         else {
-            if(stack.at(-1) === `(`) {
-                if(s[i] === `)`) stack.pop();
-                else stack.push(`(`);
-            }
-            else {
-                stack.push(`(`);
-            }
+            if (stack.length === 0) return false;
+            stack.pop();
         }
     }
-    
-    return stack.length == 0 && true;
+
+    return stack.length === 0;
 }
