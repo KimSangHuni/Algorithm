c69927d5c13b814277e9a4fe3301a6202817bbdf
KimSangHuni <158068697+KimSangHuni@users.noreply.github.com>
2024-02-07 13:57:20 +0900

    [level 2] Title: 요격 시스템, Time: 14.20 ms, Memory: 62.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/181188. 요격 시스템/요격 시스템.kt
@@ -0,0 +1,17 @@
+class Solution {
+    fun solution(targets: Array<IntArray>): Int {
+        var answer: Int = 0
+        val sortedTarget = targets.sortedBy { it[1] }
+        
+        var s = 0
+        var e = 0
+        
+        for(target in sortedTarget) {
+            if(e <= target[0]) {
+                answer += 1
+                e = target[1]
+            }
+        }
+        return answer
+    }
+}
