6f4ff5e86ed5be7c9041d2afa9216efb9ff32ec5
KimSangHuni <158068697+KimSangHuni@users.noreply.github.com>
2024-03-13 18:41:14 +0900

    [level 2] Title: 구명보트, Time: 10.42 ms, Memory: 38.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/42885. 구명보트/구명보트.js
@@ -0,0 +1,16 @@
+function solution(people, limit) {
+    people.sort((a, b) => a - b);
+
+    let count = 0;
+    let left = 0, right = people.length - 1;
+
+    while (left <= right) {
+        if (people[left] + people[right] <= limit) {
+            left++;
+        }
+        right--;
+        count++;
+    }
+
+    return count;
+}
